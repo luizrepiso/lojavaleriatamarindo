b8750af6c5b1546dd134578d738769f9aaeddde1
Luiz Repiso <luizrepiso@yahoo.es>
2022-06-22 23:10:41 -0300

    Backend project created

---
+++ backend/src/main/java/com/luizrepiso/lojavaleriatamarindo/LojavaleriatamarindoApplication.java
@@ -0,0 +1,13 @@
+package com.luizrepiso.lojavaleriatamarindo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojavaleriatamarindoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojavaleriatamarindoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/luizrepiso/lojavaleriatamarindo/LojavaleriatamarindoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizrepiso.lojavaleriatamarindo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojavaleriatamarindoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
