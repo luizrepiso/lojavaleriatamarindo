aba79edc8640467c33052af4d0a75712757a3bf5
Luiz Repiso <luizrepiso@yahoo.es>
2022-06-28 22:59:02 -0300

    Find Category by id, exception handling

--- backend/src/main/java/com/luizrepiso/lojavaleriatamarindo/services/CategoryService.java
+++ backend/src/main/java/com/luizrepiso/lojavaleriatamarindo/services/CategoryService.java
@@ -1,6 +1,7 @@
 package com.luizrepiso.lojavaleriatamarindo.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.luizrepiso.lojavaleriatamarindo.dto.CategoryDTO;
 import com.luizrepiso.lojavaleriatamarindo.entities.Category;
 import com.luizrepiso.lojavaleriatamarindo.repositories.CategoryRepository;
+import com.luizrepiso.lojavaleriatamarindo.services.exceptions.EntityNotFoundException;
 
 @Service
 public class CategoryService {
@@ -21,5 +23,13 @@ public class CategoryService {
 	public List<CategoryDTO> findAll() {
 		List<Category> list = repository.findAll();
 		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+	}
+
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		Category entity = obj.orElseThrow(() -> new EntityNotFoundException("EntityNotFound"));
+		return new CategoryDTO(entity);
+
 	}
 }
