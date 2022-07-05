c0a589b367fca5197850f9270d37ba91d6dcaec3
Luiz Repiso <luizrepiso@yahoo.es>
2022-07-04 21:59:19 -0300

    Auditing data

--- backend/src/main/java/com/luizrepiso/lojavaleriatamarindo/services/CategoryService.java
+++ backend/src/main/java/com/luizrepiso/lojavaleriatamarindo/services/CategoryService.java
@@ -7,12 +7,15 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.luizrepiso.lojavaleriatamarindo.dto.CategoryDTO;
 import com.luizrepiso.lojavaleriatamarindo.entities.Category;
 import com.luizrepiso.lojavaleriatamarindo.repositories.CategoryRepository;
+import com.luizrepiso.lojavaleriatamarindo.services.exceptions.DatabaseException;
 import com.luizrepiso.lojavaleriatamarindo.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -56,4 +59,16 @@ public class CategoryService {
 		}
 
 	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
+		}
+
+	}
+
 }
