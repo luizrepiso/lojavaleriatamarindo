fb9691bb2e85c0976d8f55a1c09b7f108d3feed1
Luiz Repiso <luizrepiso@yahoo.es>
2022-07-28 21:33:30 -0300

    Product Service

--- backend/src/main/java/com/luizrepiso/lojavaleriatamarindo/services/ProductService.java
+++ backend/src/main/java/com/luizrepiso/lojavaleriatamarindo/services/ProductService.java
@@ -12,8 +12,11 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.luizrepiso.lojavaleriatamarindo.dto.CategoryDTO;
 import com.luizrepiso.lojavaleriatamarindo.dto.ProductDTO;
+import com.luizrepiso.lojavaleriatamarindo.entities.Category;
 import com.luizrepiso.lojavaleriatamarindo.entities.Product;
+import com.luizrepiso.lojavaleriatamarindo.repositories.CategoryRepository;
 import com.luizrepiso.lojavaleriatamarindo.repositories.ProductRepository;
 import com.luizrepiso.lojavaleriatamarindo.services.exceptions.DatabaseException;
 import com.luizrepiso.lojavaleriatamarindo.services.exceptions.ResourceNotFoundException;
@@ -23,6 +26,9 @@ public class ProductService {
 
 	@Autowired
 	private ProductRepository repository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Transactional(readOnly = true)
 	public Page<ProductDTO> findAllPaged(PageRequest pageRequest) {
@@ -40,7 +46,7 @@ public class ProductService {
 
 	public ProductDTO insert(ProductDTO dto) {
 		Product entity = new Product();
-		entity.setName(dto.getName());
+		copyDtoToEntity(dto, entity);		
 		entity = repository.save(entity);
 		return new ProductDTO(entity);
 
@@ -50,7 +56,7 @@ public class ProductService {
 	public ProductDTO update(Long id, ProductDTO dto) {
 		try {
 			Product entity = repository.getReferenceById(id);
-			entity.setName(dto.getName());
+			copyDtoToEntity(dto, entity);				
 			entity = repository.save(entity);
 			return new ProductDTO(entity);
 		} catch (EntityNotFoundException e) {
@@ -70,5 +76,21 @@ public class ProductService {
 		}
 
 	}
+	
 
+	private void copyDtoToEntity(ProductDTO dto, Product entity) {
+		
+		entity.setName(dto.getName());
+		entity.setDescription(dto.getDescription());
+		entity.setDate(dto.getDate());
+		entity.setImgUrl(dto.getImgUrl());
+		entity.setPrice(dto.getPrice());
+		
+		entity.getCategories().clear();
+		for(CategoryDTO catDto : dto.getCategories()) {
+			Category category = categoryRepository.getOne(catDto.getId());
+			entity.getCategories().add(category);
+		}
+
+	}
 }
--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,4 +1,15 @@
-INSERT INTO tb_category (name, created_At) VALUES ('Blusas', NOW());
-INSERT INTO tb_category (name, created_At) VALUES ('Calças', NOW());
-INSERT INTO tb_category (name, created_At) VALUES ('Saias', NOW());
-INSERT INTO tb_category (name, created_At) VALUES ('Vestidos', NOW());
+INSERT INTO tb_category (name, created_At) VALUES ('Livros', NOW());
+INSERT INTO tb_category (name, created_At) VALUES ('Eletrônicos', NOW());
+INSERT INTO tb_category (name, created_At) VALUES ('Computadores', NOW());
+
+INSERT INTO tb_product (name, price, date, description,  img_url ) VALUES ('The Lord of the Rings', 90.5, TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z', 'fdfdfdfdfdfdf','https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/16-big.jpg');
+INSERT INTO tb_product (name, price, date, description,  img_url ) VALUES ('The Lord of the Rings', 90.5, TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z', 'fdfdfdfdfdfdf','https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/16-big.jpg');
+INSERT INTO tb_product (name, price, date, description,  img_url ) VALUES ('The Lord of the Rings', 90.5, TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z', 'fdfdfdfdfdfdf','https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/16-big.jpg');
+INSERT INTO tb_product (name, price, date, description,  img_url ) VALUES ('The Lord of the Rings', 90.5, TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z', 'fdfdfdfdfdfdf','https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/16-big.jpg');
+INSERT INTO tb_product (name, price, date, description,  img_url ) VALUES ('The Lord of the Rings', 90.5, TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z', 'fdfdfdfdfdfdf','https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/16-big.jpg');
+
+INSERT INTO tb_product_category (product_id, category_id) VALUES (1, 2);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (2, 1);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (2, 3);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (3, 3);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (4, 3);
